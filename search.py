6e78f2a5ef0ffc7697ede2d5c45b464a05eb7c88
Haseeb Mahmood <94099496+Lethaldroid@users.noreply.github.com>
2021-12-22 16:24:29 +0500

    working on multi word search now
    single word search works perfectly but but but the multiword query needs refinement

--- search.py
+++ search.py
@@ -1,3 +1,4 @@
+import functools
 import json
 import glob
 import sys
@@ -12,6 +13,8 @@ from collections import OrderedDict
 import os.path
 fp_lex = open("lexicon.json", "r")
 lexicon = json.load(fp_lex)
+fp_url = open("urls.json", "r")
+url_dic = json.load(fp_url)
 fp_inverted = open("invertedindex.json", "r")
 invertedIndex = json.load(fp_inverted)
 stop_words = set(stopwords.words("english"))
@@ -24,6 +27,12 @@ def flatten(lst):
                 yield i
         else:
             yield elem
+
+
+def intersection(lst1, lst2):
+    temp = set(lst2)
+    lst3 = [value for value in lst1 if value in temp]
+    return lst3
 
 
 def multi_dict(k, type):
@@ -53,13 +62,39 @@ start = time.time()
 wordsInSearch = search.split()
 if len(wordsInSearch) > 1:
     # multiWord(wordsInSearch)
-    print(" ")
+    wordslist = []
+    docslist = {}
+    sth = {}
+    i=0
+    search_tokens = word_tokenize(search)
+    search_tokens = [w.lower() for w in search_tokens]
+    table = str.maketrans('', '', string.punctuation)
+    search_strip = [w.translate(table) for w in search_tokens]
+    for w in search_strip:
+        if w.isalpha() and w not in stop_words:
+            x = snow_stemmer.stem(w)
+            if x in lexicon:
+                wordslist.append(lexicon[x])
+                docslist[wordslist[i]] = invertedIndex[f"{wordslist[i]}"]
+                sth = functools.reduce(set.intersection, (set(val) for val in docslist.values()))
+            i = i+1
+
+    if len(sth) == 0:
+        print("No such combination of words exist in the database")
+    else:
+        for val in sth:
+            print(url_dic[f"{val}"])
+
 else:
     stemmedWord = snow_stemmer.stem(search)
-    wordId = lexicon[stemmedWord]
-    elem = invertedIndex[str(wordId)]
-    sth = OrderedDict(sorted(elem.items(), key=lambda item: len(item[1]), reverse=True))
-    for key in sth.keys():
-        print(key)
-    #print(listoflist)
+    if stemmedWord in lexicon:
+        wordId = lexicon[stemmedWord]
+        elem = invertedIndex[str(wordId)]
+        sth = OrderedDict(sorted(elem.items(), key=lambda item: len(item[1]), reverse=True))
+        print(sth)
+        for key in sth.keys():
+            print(url_dic[f"{key}"])
+        #print(listoflist)
+    else:
+        print("No such word exists in the database")
 print(time.time()-start)
