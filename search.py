8bcb3ddc2ce1dda12943969dd42d324dacbd9ba3
Haseeb Mahmood <94099496+Lethaldroid@users.noreply.github.com>
2021-12-22 09:13:22 +0500

    Cleaner Code

--- search.py
+++ search.py
@@ -17,36 +17,6 @@ invertedIndex = json.load(fp_inverted)
 stop_words = set(stopwords.words("english"))
 snow_stemmer = SnowballStemmer(language='english')
 
-def flatten(lst):
-    for elem in lst:
-        if type(elem) in (tuple, list):
-            for i in flatten(elem):
-                yield i
-        else:
-            yield elem
-
-
-def multi_dict(k, type):
-    if k == 1:
-        return defaultdict(type)
-    else:
-        return defaultdict(lambda: multi_dict(k - 1, type))
-
-
-def check_if_string_in_file(file_name, string_to_search):
-    """ Check if any line in the file contains given string """
-    # Open the file in read only mode
-    with open(file_name, 'r') as read_obj:
-        # Read all lines in the file one by one
-        for line in read_obj:
-            # For each line, check if line contains the string
-            if string_to_search in line:
-                return True
-    return False
-
-
-write = 0
-start_time = time.time()
 
 search = input("Enter your search : ")
 start = time.time()
