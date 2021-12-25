c0684c9e153543cfe9f2690dff502492f8fe0c14
Saif Ali <69602793+saifalyy@users.noreply.github.com>
2021-12-26 04:48:18 +0500

    Now makes inverted index for Title as well

--- main.py
+++ main.py
@@ -59,11 +59,16 @@ if os.path.isfile("lexicon.json"):
     fp_fi = open("forwardindex.json", "r")
     fi_dictionary = json.load(fp_fi)
     fp_fi.close()
+
+    fp_ti = open("titleinverted.json", "r")
+    title_dictionary = json.load(fp_ti)
+    fp_ti.close()
 else:
     url_dictionary = {}
     lex_dictionary = {}
     fi_dictionary = defaultdict(list)
     ii_dictionary = multi_dict(2, list)
+    title_dictionary = defaultdict(list)
 
 fp_temp = open("storage.txt", "r")
 key = int(fp_temp.readline())
@@ -84,9 +89,12 @@ for fname in glob.glob("newsdata/*.json"):
                 position = 0
                 word_tokens = word_tokenize(y[i]["content"])
                 url_dictionary[f"{docid}"] = y[i]["url"]
+                title_tokens = word_tokenize(y[i]["title"])
                 word_tokens = [w.lower() for w in word_tokens]
+                title_tokens = [t.lower() for t in title_tokens]
                 table = str.maketrans('', '', string.punctuation)
                 strip = [w.translate(table) for w in word_tokens]
+                title_strip = [t.translate(table) for t in title_tokens]
                 for w in strip:
                     if w.isalpha() and w not in stop_words:
                         x = snow_stemmer.stem(w)
@@ -114,6 +122,18 @@ for fname in glob.glob("newsdata/*.json"):
                                 if lex_dictionary[x] not in fi_dictionary[f"{docid}"]:
                                     fi_dictionary[f"{docid}"].append(lex_dictionary[x])
                         position += 1
+                for t in title_strip:
+                    if t.isalpha() and t not in stop_words:
+                        t = snow_stemmer.stem(t)
+                    if t not in lex_dictionary:
+                        lex_dictionary[t]=key
+                        key+=1
+                    if t in lex_dictionary:
+                        if str(lex_dictionary[t]) not in title_dictionary:
+                            title_dictionary[f"{lex_dictionary[t]}"] = [docid]
+                        if str(lex_dictionary[t]) in title_dictionary:
+                            if docid not in title_dictionary[f"{lex_dictionary[t]}"]:
+                                title_dictionary[f"{lex_dictionary[t]}"].append(docid)
                 docid = docid + 1
             print(fp.name)
             print(docid)
@@ -132,13 +152,16 @@ if write == 1:
     fp_ii = open("invertedindex.json", "w")
     fp_lex = open("lexicon.json", "w")
     fp_url = open("urls.json","w")
+    fp_ti = open("titleinverted.json", "w")
     json.dump(lex_dictionary, fp_lex)
     json.dump(fi_dictionary, fp_fi)
     json.dump(ii_dictionary, fp_ii)
     json.dump(url_dictionary, fp_url)
+    json.dump(title_dictionary, fp_ti)
     fp_url.close()
     fp_ii.close()
     fp_lex.close()
+    fp_ti.close()
 
 fp_fi.close()
 fp_filenames.close()
